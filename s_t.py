ea34cd81c79c05e90b9d0e9fc2f776f76fa3c09a
sofiher04 <chofifotos@gmail.com>
2024-08-22 10:47:58 -0500

    Update s_t.py

--- s_t.py
+++ s_t.py
@@ -1,3 +1,10 @@
+#Librerías del OCR
+import streamlit as st
+import cv2
+import numpy as np
+import pytesseract
+from PIL import Image
+#Librerías del traductor
 import os
 import streamlit as st
 from bokeh.models.widgets import Button
@@ -9,8 +16,31 @@ from PIL import Image
 import time
 import glob
 
+#Lector de imagen
+st.title("Reconocimiento óptico de Caracteres")
+
+img_file_buffer = st.camera_input("Toma una Foto")
+
+with st.sidebar:
+      filtro = st.radio("Aplicar Filtro",('Con Filtro', 'Sin Filtro'))
 
 
+if img_file_buffer is not None:
+    # To read image file buffer with OpenCV:
+    bytes_data = img_file_buffer.getvalue()
+    cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
+    
+    if filtro == 'Con Filtro':
+         cv2_img=cv2.bitwise_not(cv2_img)
+    else:
+         cv2_img= cv2_img
+    
+        
+    img_rgb = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
+    text=pytesseract.image_to_string(img_rgb)
+    st.write(text)
+
+#Traductor
 from gtts import gTTS
 from googletrans import Translator
 
