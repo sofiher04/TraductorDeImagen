7fbffafd74cdaa656b8a047e8957b5f55bb0c2df
sofiher04 <chofifotos@gmail.com>
2024-08-22 10:56:19 -0500

    Update s_t.py

--- s_t.py
+++ s_t.py
@@ -8,8 +8,8 @@ from PIL import Image
 import os
 import streamlit as st
 from bokeh.models.widgets import Button
-#from bokeh.io import show
-#from bokeh.models import Button
+from bokeh.io import show
+from bokeh.models import Button
 from bokeh.models import CustomJS
 from streamlit_bokeh_events import streamlit_bokeh_events
 from PIL import Image
